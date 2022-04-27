9be2cc51fa2b53cbcb915793d7e76a3317bb92b9
sabadoryo <eabilbay@gmail.com>
2022-04-27 16:12:27 +0600

    another cors fix

--- apps/brigadir-backend/src/main.ts
+++ apps/brigadir-backend/src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const whitelist = [
+  'https://brigadir.sabadoryo.com',
+  'http://localhost:3006',
+  undefined,
+];
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*',
+  await app.enableCors({
+    origin: (origin, callback) => {
+      if (whitelist.includes(origin)) {
+        callback(null, true);
+      } else {
+        console.log('error');
+        callback(new Error('not allowed by origin'));
+      }
+    },
   });
   await app.listen(3000);
 }
